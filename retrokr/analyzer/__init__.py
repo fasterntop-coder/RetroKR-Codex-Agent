dc49260caf5dfa90bdafb65c707d0beb3613111c
fasterntop-coder <fasterntop@gmail.com>
2026-07-03 07:08:56 +0900

    Update __init__.py

--- retrokr/analyzer/__init__.py
+++ retrokr/analyzer/__init__.py
@@ -1,7 +1,15 @@
 """ROM analyzer package."""
 
 from retrokr.analyzer.console import ConsoleCandidate, ConsoleDetector
+from retrokr.analyzer.encoding import EncodingCandidate, EncodingDetector
 from retrokr.analyzer.models import RomInfo
 from retrokr.analyzer.rom import RomLoader
 
-__all__ = ["ConsoleCandidate", "ConsoleDetector", "RomInfo", "RomLoader"]
+__all__ = [
+    "ConsoleCandidate",
+    "ConsoleDetector",
+    "EncodingCandidate",
+    "EncodingDetector",
+    "RomInfo",
+    "RomLoader",
+]
